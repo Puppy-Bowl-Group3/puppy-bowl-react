e8d13929dbfb2857de98f121dd1518fca19a5252
Megan Chiu <megan.chiu.2014@gmail.com>
2025-02-13 09:34:51 -0800

    broken import js function code

---
+++ src/API/index.js
@@ -0,0 +1,23 @@
+const cohortName = "2410-ftb-et-web-am";
+const PLAYERS_API_URL = `https://fsa-puppy-bowl.herokuapp.com/api/${cohortName}/players`;
+
+// Initialize empty players & teams arrays
+const state = {
+  players:[],
+  teams:[],
+}
+
+/**
+ * Fetches all players from the API.
+ * @returns {Object[]} the array of player objects
+ */
+export default fetchAllPlayers = async () => {
+  try {
+    const response = await fetch(PLAYERS_API_URL);
+    const data = await response.json();
+    state.players = data.data;
+    return state.players;
+  } catch (err) {
+    console.error("Uh oh, trouble fetching players!", err);
+  }
+};
--- src/components/AllPlayers.jsx
+++ src/components/AllPlayers.jsx
@@ -1,4 +1,11 @@
-export default function AllPlayers() {
+import {fetchAllPlayers} from '../API/index.js'
+import { useState, useEffect } from 'react'
+
+export function AllPlayers() {
+  const [players, setPlayers] = useState([])
+
+  console.log({fetchAllPlayers});
+
   return (
     <>
       <div>
