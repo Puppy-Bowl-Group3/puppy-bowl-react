921d8cb4d748430c89ee979b05389eb427e7376c
sandpitt-turtle <wottkowicz@gmail.com>
2025-02-16 11:20:05 -0500

    SearchBar added and CSS applied.

--- src/components/AllPlayers.jsx
+++ src/components/AllPlayers.jsx
@@ -1,10 +1,13 @@
-import { fetchAllPlayers } from '../API/index.js'
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react';
+import { fetchAllPlayers } from '../API/index.js';
 import SinglePlayer from './SinglePlayer.jsx';
 import NewPlayerForm from './NewPlayerForm.jsx';
+import SearchBar from './SearchBar';
 
-export function AllPlayers({allNewPlayers, setAllNewPlayers}) {
-const [players, setPlayers] = useState([])  
+export function AllPlayers({ allNewPlayers, setAllNewPlayers }) {
+  const [players, setPlayers] = useState([]);
+  const [filteredPlayers, setFilteredPlayers] = useState([]);
+  const defaultImage = 'https://imgur.com/Im9JnQI.png';
 
   useEffect(() => {
     const getPlayers = async () => {
@@ -12,32 +15,34 @@ const [players, setPlayers] = useState([])
         const data = await fetchAllPlayers();
         console.log('data => ', data);
         setPlayers(data.players);
+        setFilteredPlayers(data.players);
       } catch (error) {
-        console.error(error)
+        console.error(error);
       }
     };
     getPlayers();
   }, [allNewPlayers]);
 
-  const defaultImage = "https://imgur.com/Im9JnQI.png"; 
-
-  // Use this to make sure we are calling the function correctly
-  // console.log({fetchAllPlayers});
+  const handleSearch = (query) => {
+    const filtered = players.filter((player) =>
+      player.name.toLowerCase().includes(query.toLowerCase())
+    );
+    setFilteredPlayers(filtered);
+  };
 
   return (
     <>
-    <NewPlayerForm setAllNewPlayers={setAllNewPlayers}/>
-    <div className='playersCard'>
-        {players.map((player) => (
-              //console.log('player card player = ', player)
-              <div key={player.id} className="playersCard">
-              <h4><b>Name:</b> {player.name}</h4>
-              <h4><b>Breed:</b> {player.breed}</h4>
-
-                        <img 
-              className="playerImg" 
+      <NewPlayerForm setAllNewPlayers={setAllNewPlayers} />
+      <SearchBar onSearch={handleSearch} />
+      <div className='playersCard'>
+        {filteredPlayers.map((player) => (
+          <div key={player.id} className='playersCard'>
+            <h4><b>Name:</b> {player.name}</h4>
+            <h4><b>Breed:</b> {player.breed}</h4>
+            <img 
+              className='playerImg' 
               src={player.imageUrl?.trim() ? player.imageUrl : defaultImage} 
-              alt={player.name || "Default Puppy"}
+              alt={player.name || 'Default Puppy'}
               onError={(e) => (e.target.src = defaultImage)}
             />
             <SinglePlayer playerId={player.id} />
