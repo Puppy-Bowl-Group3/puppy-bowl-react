5d37acb7e2ad1e8f80a8cc9a59aeade5f4493991
sandpitt-turtle <wottkowicz@gmail.com>
2025-02-16 09:50:22 -0500

    SearchBar.jsx added. AllPlayers.jsx edited to handle SearchBar. Added basic SearchBar styling to App.css

--- src/components/AllPlayers.jsx
+++ src/components/AllPlayers.jsx
@@ -1,10 +1,13 @@
-import {fetchAllPlayers, fetchSinglePlayer} from '../API/index.js'
 import React, { useState, useEffect } from 'react'
+import {fetchAllPlayers, fetchSinglePlayer} from '../API/index.js'
 import { Route, useNavigate } from 'react-router-dom';
 import SinglePlayer from './SinglePlayer.jsx';
+import SearchBar from "./SearchBar";
+
 
 export function AllPlayers() {
   const [players, setPlayers] = useState([])
+  const [filteredPlayers, setFilteredPlayers] = useState([]);
 
   useEffect((e) => {
     const getPlayers = async () => {
@@ -12,6 +15,7 @@ export function AllPlayers() {
         const data = await fetchAllPlayers();
         console.log('data => ', data);
         setPlayers(data.players);
+        setFilteredPlayers(data.players);
       } catch (error) {
         console.error(error)
       }
@@ -19,27 +23,32 @@ export function AllPlayers() {
     getPlayers();
   }, []);
 
+
+  const handleSearch = (query) => {
+    const filtered = players.filter((player) =>
+      player.name.toLowerCase().includes(query.toLowerCase())
+    );
+    setFilteredPlayers(filtered);
+  };
+
   // Use this to make sure we are calling the function correctly
   // console.log({fetchAllPlayers});
 
   return (
     <>
-    <div className='playersCard'>
-        {
-            players.map((player)=>{
-              console.log('player card player = ', player)
-                return (
-                    <div key={player.id}>
-                        <h4>{player.name}</h4>
-                        <h4>{player.breed}</h4>
-                        <img className='playerImg' src={player.imageUrl} alt={player.name}/><br/>
-                        <SinglePlayer playerId={player.id} />
-                        
-                    </div>
-                )
-            })
-        }
-    </div>
+      <SearchBar onSearch={handleSearch} /> 
+      <div className='playersCard'>
+        {filteredPlayers.map((player) => ( 
+
+          <div key={player.id}>
+            <h4>{player.name}</h4>
+            <h4>{player.breed}</h4>
+            <img className='playerImg' src={player.imageUrl} alt={player.name} /><br />
+            <SinglePlayer playerId={player.id} />
+          </div>
+          
+        ))}
+      </div>
     </>
   );
 }
